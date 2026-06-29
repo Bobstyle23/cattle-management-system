da894796b68c528a719cf10c37fde86940bd6824
Mukhammad Bobur Pakhriev <boburpakhriev@gmail.com>
2026-06-29 16:21:51 +0900

    add creating a cattle api

--- src/app/cattle/new/page.tsx
+++ src/app/cattle/new/page.tsx
@@ -1,11 +1,26 @@
+"use client";
+
 import CattleForm from "@/components/cattle/CattleForm";
+import { createCattle } from "@/services/cattle";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
 
 export default function AddCattlePage() {
+  const queryClient = useQueryClient();
+
+  const mutation = useMutation({
+    mutationFn: createCattle,
+    onSuccess() {
+      queryClient.invalidateQueries({
+        queryKey: ["cattle"],
+      });
+    },
+  });
+
   return (
     <>
       <h2 className="text-2xl font-bold mb-6">Add Cattle</h2>
 
-      <CattleForm />
+      <CattleForm onSubmit={(values) => mutation.mutate(values)} />
     </>
   );
 }
--- src/services/cattle.ts
+++ src/services/cattle.ts
@@ -1,8 +1,28 @@
+import { CattleFormValues } from "@/entities/CattleFormValues";
+
 export async function getCattle() {
   const response = await fetch("/api/cattle");
 
   if (!response.ok) {
     throw new Error("Failed to fetch cattle");
+  }
+
+  return response.json();
+}
+
+export async function createCattle(data: CattleFormValues) {
+  const response = await fetch("/api/cattle", {
+    method: "POST",
+
+    headers: {
+      "Content-Type": "application/json",
+    },
+
+    body: JSON.stringify(data),
+  });
+
+  if (!response.ok) {
+    throw new Error("Failed to create cattle");
   }
 
   return response.json();
